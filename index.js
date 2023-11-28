c71b4ca66a8db85f3a8845d7d423c5f045d5f246
dunglq3110 <144051000+dunglq3110@users.noreply.github.com>
2023-11-28 20:04:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const port = 3000
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('draw.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
