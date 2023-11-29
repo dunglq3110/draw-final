e8098741aa00e422163e464d0d09b228dd12cf1d
powoftech <powoftech@gmail.com>
2023-11-29 19:41:15 +0700

    Reformat index.js, draw.ejs. Refactor .gitignore.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const app = express()
-const path = require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const port = 3000
+const PORT = 3000;
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('draw.ejs')
-})
+app.get("/", (req, res) => {
+	res.render("draw.ejs");
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+	console.log(`Example app listening on port ${PORT}`);
+});
+
+exports.PORT = PORT
