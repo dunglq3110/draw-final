6ebad8c1a3ec6f4717f9ce21008fb9c8e898009a
powoftech <powoftech@gmail.com>
2023-11-30 15:14:40 +0700

    Refactor tool options and update default values for "cloud" and "draw" tools.

--- frontend/public/js/tools/draw.js
+++ frontend/public/js/tools/draw.js
@@ -2,68 +2,68 @@
 // adapted from resources on http://paperjs.org
 
 pg.tools.registerTool({
-	id: 'draw',
-	name: 'Draw'
+	id: "draw",
+	name: "Draw",
 });
 
-pg.tools.draw = function() {
+pg.tools.draw = function () {
 	var tool;
-	
+
 	var options = {
-		pointDistance: 20,
+		pointDistance: 1,
 		drawParallelLines: false,
 		lines: 3,
 		lineDistance: 10,
-		closePath: 'near start',
-		smoothPath : true
+		closePath: "never",
+		smoothPath: true,
 	};
-	
+
 	var components = {
 		pointDistance: {
-			type: 'int',
-			label: 'Point distance',
-			min: 1
+			type: "int",
+			label: "Point distance",
+			min: 1,
 		},
 		drawParallelLines: {
-			type: 'boolean',
-			label: 'Draw parallel lines'
+			type: "boolean",
+			label: "Draw parallel lines",
 		},
 		lines: {
-			type: 'int',
-			label: 'Lines',
-			requirements : {drawParallelLines: true},
-			min: 1
+			type: "int",
+			label: "Lines",
+			requirements: { drawParallelLines: true },
+			min: 1,
 		},
 		lineDistance: {
-			type: 'float',
-			label: 'Line distance',
-			requirements : {drawParallelLines: true},
-			min: 0
+			type: "float",
+			label: "Line distance",
+			requirements: { drawParallelLines: true },
+			min: 0,
 		},
 		closePath: {
-			type: 'list',
-			label: 'Close path',
-			options: [ 'near start', 'always', 'never' ]
+			type: "list",
+			label: "Close path",
+			options: ["near start", "always", "never"],
 		},
 		smoothPath: {
-			type: 'boolean',
-			label: 'Smooth path'
-		}
+			type: "boolean",
+			label: "Smooth path",
+		},
 	};
 
-	var activateTool = function() {
+	var activateTool = function () {
 		var paths = [];
-		
+
 		// get options from local storage if present
 		options = pg.tools.getLocalOptions(options);
-		
+
 		tool = new Tool();
-		
+
 		var lineCount;
 
-		tool.onMouseDown = function(event) {
-			if(event.event.button > 0) return;  // only first mouse button
-			
+		tool.onMouseDown = function (event) {
+			if (event.event.button > 0) return; // only first mouse button
+
 			tool.fixedDistance = options.pointDistance;
 
 			if (options.drawParallelLines) {
@@ -71,76 +71,72 @@ pg.tools.draw = function() {
 			} else {
 				lineCount = 1;
 			}
-		
-			for( var i=0; i < lineCount; i++) {
+
+			for (var i = 0; i < lineCount; i++) {
 				var path = paths[i];
 				path = new Path();
-				
+
 				path = pg.stylebar.applyActiveToolbarStyle(path);
-				
+
 				paths.push(path);
 			}
 		};
 
-		tool.onMouseDrag = function(event) {
-			if(event.event.button > 0) return;  // only first mouse button
-						
+		tool.onMouseDrag = function (event) {
+			if (event.event.button > 0) return; // only first mouse button
+
 			var offset = event.delta;
 			offset.angle += 90;
-			for( var i=0; i < lineCount; i++) {
+			for (var i = 0; i < lineCount; i++) {
 				var path = paths[i];
 				offset.length = options.lineDistance * i;
 				path.add(event.middlePoint + offset);
 			}
 		};
 
-		tool.onMouseUp = function(event) {
-			if(event.event.button > 0) return;  // only first mouse button
+		tool.onMouseUp = function (event) {
+			if (event.event.button > 0) return; // only first mouse button
 			//
 			// accidental clicks produce a path but no segments
 			// so return if an accidental click happened
-			if(paths[0].segments.length === 0) return;
-			
+			if (paths[0].segments.length === 0) return;
+
 			var group;
-			if(lineCount > 1) {
+			if (lineCount > 1) {
 				group = new Group();
 			}
-			
+
 			var nearStart = pg.math.checkPointsClose(paths[0].firstSegment.point, event.point, 30);
-			for( var i=0; i < lineCount; i++) {
+			for (var i = 0; i < lineCount; i++) {
 				var path = paths[i];
-				
-				if(options.closePath === 'near start' && nearStart) {
+
+				if (options.closePath === "near start" && nearStart) {
 					path.closePath(true);
-				} else if(options.closePath === 'always') {
+				} else if (options.closePath === "always") {
 					path.closePath(true);
 				}
-				if(options.smoothPath) path.smooth();
-				
-				if(lineCount > 1) {
+				if (options.smoothPath) path.smooth();
+
+				if (lineCount > 1) {
 					group.addChild(path);
 				}
 			}
-			
+
 			paths = [];
-			pg.undo.snapshot('draw');
-			
+			pg.undo.snapshot("draw");
 		};
-		
+
 		// setup floating tool options panel in the editor
-		pg.toolOptionPanel.setup(options, components, function() {
+		pg.toolOptionPanel.setup(options, components, function () {
 			lineCount = options.lines;
 			tool.fixedDistance = options.pointDistance;
 		});
-		
-		tool.activate();
-		
-	};
 
+		tool.activate();
+	};
 
 	return {
 		options: options,
-		activateTool:activateTool
+		activateTool: activateTool,
 	};
-
 };
